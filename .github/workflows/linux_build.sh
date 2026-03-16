c02091ee8cd3c620da0b73025d52ed1fde2e4976
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2026-03-16 03:50:44 -0400

    CI/CD Changes

--- .github/workflows/linux_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-# This is an auxiliary script to build and test SAMSEG wheels
-PYTHON_PATH=$1
-$PYTHON_PATH -m pip wheel . -w ./dist  --no-deps
-$PYTHON_PATH -m pip install samseg -f dist/
-$PYTHON_PATH -m pip install pytest
-$PYTHON_PATH -m pip install tensorflow
-$PYTHON_PATH -m pytest samseg/tests
-rm samseg/gems/*.so 
