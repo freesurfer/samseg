7793074c6b5439d00079a1e7adf243f2665802e3
Jackson Nolan <77302298+jnolan14@users.noreply.github.com>
2026-09-22 16:21:32 -0400

    Update setup.py
    Cleanup of some debug print statements

--- setup.py
+++ setup.py
@@ -13,8 +13,6 @@ import tempfile
 # Replace build-ext to run CMake in order to build the bindings
 class build_ext_(build_ext):
     def run(self):
-        print("+++++++++++++++++++++++++++\nBEGIN SETUP RUN CALL\n+++++++++++++++++++++++++++\n")
-        print(f"RUNNING ON PLATFORM: {sys.platform}")
         package_root = os.path.abspath(os.path.dirname(__file__))
         # Run CMAKE
         with tempfile.TemporaryDirectory() as tmpdir:
@@ -33,8 +31,6 @@ class build_ext_(build_ext):
                     pass
                 else:
                     cmake_call += [f'-D{k}={path}']
-            print(' '.join(cmake_call))
-            print(f"+++++++++++++++++++++++++++\nCMAKE_CMD: {cmake_call}\n+++++++++++++++++++++++++++\n")
             subprocess.run(cmake_call, check=True)
             # Run Make
             if sys.platform == 'win32':
